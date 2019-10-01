0e5c3e9f32ad351866f710d42b4d81bef81d1087
Bond <jobocuban@gmail.com>
2019-10-01 14:33:30 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.blackjack.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.blackjack.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/blackjack/model/Shoe.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.blackjack.model;
+
+public class Shoe {
+
+}
